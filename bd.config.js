b5dffedb1546c3c6303df061eabd502045af0c11
Ignacio Montaña <ignaciom8719@gmail.com>
2022-08-24 12:21:33 -0300

    feat: heroku database connection addesd

--- bd.config.js
+++ bd.config.js
@@ -1,11 +1,11 @@
 import mysql2 from "mysql2/promise";
 
 const pool = mysql2.createPool({
-    host: "localhost",
+    host: "us-cdbr-east-06.cleardb.net",
     port: 3306,
-    user: "root",
-    password: "ignacio",
-    database: "proyectofullstack"
+    user: "b258fa959b58a6",
+    password: "4ceeaf90",
+    database: "heroku_0ea0b5d2aa8d7ac"
 });
 
 export default pool;
