80582e6ef9e0f412033f1ff6755aa78efefe1850
Ignacio Montaña <ignaciom8719@gmail.com>
2022-08-24 12:24:14 -0300

    fix: database connection port removed

--- bd.config.js
+++ bd.config.js
@@ -2,7 +2,6 @@ import mysql2 from "mysql2/promise";
 
 const pool = mysql2.createPool({
     host: "us-cdbr-east-06.cleardb.net",
-    port: 3306,
     user: "b258fa959b58a6",
     password: "4ceeaf90",
     database: "heroku_0ea0b5d2aa8d7ac"
